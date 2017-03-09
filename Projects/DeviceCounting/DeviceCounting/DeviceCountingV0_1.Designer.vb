c64d22d34fb375d03944d32c43d9ba00db8ad640
StevenKelleher <skelleher@x-celeprint.com>
2017-03-09 11:45:32 +0000

    Initial Commit

---
+++ Projects/DeviceCounting/DeviceCounting/DeviceCountingV0_1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DeviceCountingV0_1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(DeviceCountingV0_1))
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderBrowserDialog2 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(369, 219)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(114, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Score using clutter"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 278)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 45)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Run"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 338)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(502, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(121, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(78, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(6, 19)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {1410065407, 2, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(122, 20)
+        Me.NumericUpDown1.TabIndex = 6
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.DecimalPlaces = 2
+        Me.NumericUpDown2.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.NumericUpDown2.Location = New System.Drawing.Point(6, 19)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 131072})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(122, 20)
+        Me.NumericUpDown2.TabIndex = 7
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {50, 0, 0, 131072})
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(6, 19)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {1410065407, 2, 0, 0})
+        Me.NumericUpDown3.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(102, 20)
+        Me.NumericUpDown3.TabIndex = 8
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox6)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(351, 329)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Device Image"
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.NumericUpDown3)
+        Me.GroupBox6.Location = New System.Drawing.Point(231, 280)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(114, 43)
+        Me.GroupBox6.TabIndex = 13
+        Me.GroupBox6.TabStop = False
+        Me.GroupBox6.Text = "Device Total"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(113, 279)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(112, 43)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Device Yield"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(90, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "%"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox3.Location = New System.Drawing.Point(363, 165)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(133, 48)
+        Me.GroupBox3.TabIndex = 10
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Devices to find"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.PictureBox2)
+        Me.GroupBox4.Location = New System.Drawing.Point(363, 6)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(133, 153)
+        Me.GroupBox4.TabIndex = 11
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Coxnex model"
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.NumericUpDown2)
+        Me.GroupBox5.Location = New System.Drawing.Point(363, 242)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(133, 52)
+        Me.GroupBox5.TabIndex = 12
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Accept Threshold"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.DeviceCounting.My.Resources.Resources.X_Celeprint_icon
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.ErrorImage = Global.DeviceCounting.My.Resources.Resources.Hamster_Error
+        Me.PictureBox2.InitialImage = Global.DeviceCounting.My.Resources.Resources.Load_Pattern1
+        Me.PictureBox2.Location = New System.Drawing.Point(6, 19)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(122, 129)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.DeviceCounting.My.Resources.Resources.X_Celeprint_icon
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.ErrorImage = Global.DeviceCounting.My.Resources.Resources.Hamster_Error
+        Me.PictureBox1.InitialImage = Global.DeviceCounting.My.Resources.Resources.Load_Image
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 19)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(339, 254)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'DeviceCountingV0_1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(502, 360)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "DeviceCountingV0_1"
+        Me.Text = "Device Count"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox5.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NumericUpDown3 As NumericUpDown
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents FolderBrowserDialog2 As FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ Projects/DeviceCounting/DeviceCounting/DeviceCountingV0_2.vb
@@ -0,0 +1,145 @@
+﻿
+Imports Cognex.VisionPro
+Imports Cognex.VisionPro.ImageProcessing
+Imports Cognex.VisionPro.PMAlign
+Imports Cognex.VisionPro.ImageFile
+Imports System.IO
+Imports Cognex.VisionPro.Display
+
+Public Class DeviceCountingV0_2
+
+
+    Dim FilePath As DialogResult
+    Dim S_PMAlign As New CogPMAlignTool
+    Dim NewImageFile As New CogImageFile
+    Dim ImageConverter As New CogImageConvertTool
+    Dim FileOpened As Boolean = False
+    Dim FileOpened2 As Boolean = False
+    Dim LPattern As Bitmap
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        PictureBox1.Image = PictureBox1.InitialImage
+        PictureBox2.Image = PictureBox2.InitialImage
+
+        FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal
+        FolderBrowserDialog1.ShowNewFolderButton = True
+        FolderBrowserDialog2.RootFolder = Environment.SpecialFolder.Personal
+        FolderBrowserDialog2.ShowNewFolderButton = True
+        OpenFileDialog1.Multiselect = True
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim Count As Integer = ImageList1.Images.Count
+        Dim n As Integer
+        If n = Count Then
+
+            NewImageFile.Open(ImageList1.Images.Item(n).ToString, CogImageFileModeConstants.Read)
+
+            If (CheckBox1.Checked = True) Then
+                S_PMAlign.RunParams.ScoreUsingClutter = True
+            Else
+                S_PMAlign.RunParams.ScoreUsingClutter = False
+            End If
+            Try
+                ImageConverter.InputImage = NewImageFile(0)
+                ImageConverter.RunParams.RunMode = CogImageConvertRunModeConstants.Intensity
+                ImageConverter.Run()
+
+                ToolStripStatusLabel1.Text = ImageConverter.RunStatus.Message
+
+                S_PMAlign.InputImage = ImageConverter.OutputImage
+                S_PMAlign.RunParams.AcceptThreshold = NumericUpDown2.Value
+                S_PMAlign.RunParams.ApproximateNumberToFind = NumericUpDown1.Value
+
+                S_PMAlign.Run()
+
+                ToolStripStatusLabel1.Text = S_PMAlign.RunStatus.Message
+
+                TextBox1.Text = (S_PMAlign.Results.Count / NumericUpDown3.Value) * 100
+
+            Catch exp As Exception
+
+                If (Not FileOpened) Then
+                    MessageBox.Show("Please load Image and Model files" _
+                                    + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine)
+                End If
+
+                FileOpened = False
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        OpenFileDialog2.Filter = "VPP File (.vpp) |*.vpp"
+
+        If (Not FileOpened2) Then
+            OpenFileDialog2.InitialDirectory = FolderBrowserDialog2.RootFolder
+            OpenFileDialog2.FileName = Nothing
+
+        End If
+
+        Try
+
+            If OpenFileDialog2.ShowDialog() = DialogResult.OK Then
+
+                Dim SavePattern As New CogPMAlignPattern
+                SavePattern = CogSerializer.LoadObjectFromFile(OpenFileDialog2.FileName)
+                S_PMAlign.Pattern = CogSerializer.LoadObjectFromFile(OpenFileDialog2.FileName)
+                LPattern = SavePattern.GetTrainedPatternImage.ToBitmap()
+                PictureBox2.Image = LPattern
+                ToolStripStatusLabel1.Text = S_PMAlign.RunStatus.Message
+            End If
+
+            FileOpened2 = True
+
+        Catch exp As Exception
+            MessageBox.Show("An error occurred while attempting to load the file. The error is:" _
+                               + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine)
+            PictureBox2.Image = PictureBox2.ErrorImage
+            FileOpened2 = False
+        End Try
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        OpenFileDialog1.Filter = "Bitmap image (.bmp) |*.bmp"
+
+        If (Not FileOpened) Then
+            OpenFileDialog1.InitialDirectory = FolderBrowserDialog1.RootFolder
+            OpenFileDialog1.FileName = Nothing
+        End If
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim file As String
+
+            For Each file In OpenFileDialog1.FileNames
+                Try
+                    Dim LoadedImage As Image = Image.FromFile(file)
+                    ImageList1.Images.Add(file, LoadedImage)
+
+
+
+                    ' Dim LImage As New StreamReader(OpenFileDialog1.FileName)
+
+                    PictureBox1.Load(OpenFileDialog1.FileName)
+                    'LImage.Close()
+
+
+                    FileOpened = True
+
+                Catch exp As Exception
+                    MessageBox.Show("An error occurred while attempting to load the file. The error is:" _
+                                        + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine)
+                    FileOpened = False
+                    PictureBox1.Image = PictureBox1.ErrorImage
+                End Try
+            Next file
+        End If
+    End Sub
+
+
+End Class
